e121031991c0aa69c87dabedffea234a42144ae4
devluish <luis_unn@hotmail.com>
2024-12-06 10:51:50 -0300

    Exercicio

---
+++ src/Desafio3/Exercicio4.java
@@ -0,0 +1,23 @@
+/*
+Crie um programa que solicite ao usuário um número e exiba a tabuada desse número de 1 a 10.
+ */
+
+package Desafio3;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número de 1 a 10");
+        int escolha = scanner.nextInt();
+
+        System.out.println("Tabuada do " + escolha);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(escolha + " * " + i + " = "+ (escolha * i));
+        }
+
+    }
+}
