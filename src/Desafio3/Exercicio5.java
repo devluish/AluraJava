ff1f18a04ab5c38af09391f1147b831096ffc09e
devluish <luis_unn@hotmail.com>
2024-12-06 11:17:04 -0300

    Exercicio

--- src/Desafio3/Exercicio5.java
+++ src/Desafio3/Exercicio5.java
@@ -1,10 +1,23 @@
+/*
+Crie um programa que solicite ao usuário a entrada de um número inteiro. Verifique se o número é par ou ímpar e exiba uma mensagem correspondente.
+ */
+
 package Desafio3;
 
-
-
+import java.util.Scanner;
 
 public class Exercicio5 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número");
+
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0){
+            System.out.println("O número: " + numero + " é par");
+        } else {
+            System.out.println("O número: " + numero + " é impar");
+        }
     }
 }
 
